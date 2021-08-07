e4c60958f7ffe4d970a0ec8bbf2230c8a1c15af2
Manish <itzmanish108@gmail.com>
2021-08-07 17:11:23 +0530

    add password utils

---
+++ utils/password.go
@@ -0,0 +1,72 @@
+package utils
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type PasswordConfig struct {
+	time    uint32
+	memory  uint32
+	threads uint8
+	keyLen  uint32
+}
+
+var DefaultPasswordConfig *PasswordConfig = &PasswordConfig{
+	time:    1,
+	memory:  64 * 1024,
+	threads: 4,
+	keyLen:  32,
+}
+
+// GeneratePassword is used to generate a new password hash for storing and
+// comparing at a later date.
+func GeneratePassword(password string) (string, error) {
+	// Generate a Salt
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+	hash := argon2.IDKey([]byte(password), salt, DefaultPasswordConfig.time, DefaultPasswordConfig.memory, DefaultPasswordConfig.threads, DefaultPasswordConfig.keyLen)
+
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	format := "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s"
+	full := fmt.Sprintf(format, argon2.Version, DefaultPasswordConfig.memory, DefaultPasswordConfig.time, DefaultPasswordConfig.threads, b64Salt, b64Hash)
+	return full, nil
+}
+
+// ComparePassword is used to compare a user-inputted password to a hash to see
+// if the password matches or not.
+func ComparePassword(password, hash string) (bool, error) {
+
+	parts := strings.Split(hash, "$")
+
+	c := &PasswordConfig{}
+	_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &c.memory, &c.time, &c.threads)
+	if err != nil {
+		return false, err
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+	if err != nil {
+		return false, err
+	}
+
+	decodedHash, err := base64.RawStdEncoding.DecodeString(parts[5])
+	if err != nil {
+		return false, err
+	}
+	c.keyLen = uint32(len(decodedHash))
+
+	comparisonHash := argon2.IDKey([]byte(password), salt, c.time, c.memory, c.threads, c.keyLen)
+
+	return (subtle.ConstantTimeCompare(decodedHash, comparisonHash) == 1), nil
+}
