e56ac93a77db4dc1f96d5e8681afc9e3a2ed7f7a
Manish <itzmanish108@gmail.com>
2021-09-05 12:34:09 +0530

    utils: for opening browser with url

---
+++ cmd/utils/openBrowser.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func Openbrowser(url string) error {
+	var err error = nil
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	return err
+}
