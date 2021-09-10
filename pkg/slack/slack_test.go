1805e7b0bbbb66ecdc0a438f6f52753a5337cc47
Manish <itzmanish108@gmail.com>
2021-09-10 13:56:45 +0530

    fix slack test

--- pkg/slack/slack_test.go
+++ pkg/slack/slack_test.go
@@ -25,10 +25,11 @@ func TestGetUserInfo(t *testing.T) {
 	log.Printf("%+v", user)
 }
 func TestSlackToken(t *testing.T) {
+	redirect_uri := "https://localhost:8080/slatomate/authorizeOrganization?user_id=fe2e4817-cd86-427a-9522-b8ff52dab0a1&org_id=983fffac-62e9-4366-9ebc-9028f2dadf2f"
 	token, err := slack.GetOAuthV2Response(http.DefaultClient,
 		"1001856848789.2347720282836",
 		"13160e61a6257cdb40006d149231aabe",
-		"1001856848789.2416145373863.f193daef6d76fe58dc9de0aa480bf01ab98384b97b9d2635c4879bbe33ebc371",
-		"")
+		"1001856848789.2469830765286.b8d670e72fa6f04ae0898717aa11921051b8343b493c68bf0d8cdf2f1735928e",
+		redirect_uri)
 	log.Println(token, err)
 }
